ffbe952a8f1d7819bcb0ef4640a9799797e25c84
hiimstevejin <jingle5695@gmail.com>
2025-02-27 15:43:51 -0500

    add login form to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,44 @@
-import Navbar from "../components/Navbar"
+import { useState } from "react";
+import GoogleLogin from "../components/GoogleLogin";
+import Navbar from "../components/Navbar/Navbar";
 
 function Login() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <>
-    <Navbar/>
-    </>
-  )
+    <main>
+      <Navbar />
+      <form>
+        <div>
+          <label htmlFor="userName">UserName</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="username"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
+      <GoogleLogin />
+      <h1>{username}</h1>
+      <h1>{password}</h1>
+    </main>
+  );
 }
 
-export default Login
+export default Login;
