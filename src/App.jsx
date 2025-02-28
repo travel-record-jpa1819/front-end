04787620baf8f38a6b3c23d7d75b0531352b38ff
hiimstevejin <jingle5695@gmail.com>
2025-02-28 18:38:01 -0500

    add routing for login, profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./pages/Profile";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/" element={<Login />} />
+        <Route path="login" element={<Login />}></Route>
+        <Route path="profile" element={<Profile />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
   );
