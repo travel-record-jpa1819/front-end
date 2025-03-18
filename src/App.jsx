9ac31134114c3e99303705781d0e7da0e92b35f1
hiimstevejin <jingle5695@gmail.com>
2025-03-17 23:19:07 -0400

    update: routing & dummy json to fetch cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useEffect, useState} from "react";
+
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard"
-import CountryList from "./components/CountryList";
+import Dashboard from "./pages/Dashboard";
+import CountryList from "./components/Dashboard/CountryList";
+import CityList from "./components/Dashboard/CityList";
+import Form from "./components/Dashboard/Form";
+import City from "./components/Dashboard/City";
 
 const googleLoginUrl = import.meta.env.VITE_GOOGLE_LOGIN_URL;
+const BASE_URL = "http://localhost:8000";
 
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There is an error fetching cities");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <Router>
       <Routes>
@@ -15,11 +45,24 @@ export default function App() {
           path="/"
           element={<Home googleLoginUrl={googleLoginUrl} />}
         ></Route>
-        <Route path="dashboard" element={<Dashboard/>}>
-          <Route index element={<CountryList/>}/>
-            <Route path="countries" element={<CountryList/>}/>
-            <Route path="profile" element={<Profile/>}/>
+        
+        <Route path="dashboard" element={<Dashboard />}>
+        <Route
+            index
+            element={<Navigate to="cities" replace/>}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form/>} />
         </Route>
+        {/* <Route path="*" element={<PageNotFound />}></Route> */}
       </Routes>
     </Router>
   );
