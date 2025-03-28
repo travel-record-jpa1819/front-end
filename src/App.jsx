aff285185e894c2f4e9edc69a28e2fa4e6ee8238
hiimstevejin <jingle5695@gmail.com>
2025-03-27 21:27:01 -0400

    refactor(context): create global cities context to use city across app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
@@ -15,67 +14,45 @@ import Form from "./components/Dashboard/Form";
 import City from "./components/Dashboard/City";
 import ProtectedRoute from "./components/ProtectedRoute";
 
+import { CitiesProvider } from "./context/CitiesContext";
+
 const googleLoginUrl = import.meta.env.VITE_GOOGLE_LOGIN_URL;
-const BASE_URL = "http://localhost:8000";
 
 export default function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There is an error fetching cities");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        ></Route>
-        <Route
-          path="/"
-          element={<Home googleLoginUrl={googleLoginUrl} />}
-        ></Route>
+    <CitiesProvider>
+      <Router>
+        <Routes>
+          <Route
+            path="profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          ></Route>
+          <Route
+            path="/"
+            element={<Home googleLoginUrl={googleLoginUrl} />}
+          ></Route>
 
-        <Route
-          path="dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Navigate to="cities" replace />} />
           <Route
-            path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path="cities/:id" element={<City />} />
-          <Route
-            path="countries"
-            element={<CountryList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path="form" element={<Form />} />
-        </Route>
-        {/* <Route path="*" element={<PageNotFound />}></Route> */}
-      </Routes>
-    </Router>
+            path="dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Navigate to="cities" replace />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          {/* <Route path="*" element={<PageNotFound />}></Route> */}
+        </Routes>
+      </Router>
+    </CitiesProvider>
   );
 }
---
+++ src/context/CitiesContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+const BASE_URL = "http://localhost:8000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There is an error fetching cities");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("Cities Context was used out side the CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
